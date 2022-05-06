1641d95b4c28902368b792c9d224ead7f1133058
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-05-05 19:26:50 -0500

    Funciones Flecha

---
+++ main.js
@@ -0,0 +1,12 @@
+// http://www.etnassoft.com/2016/06/22/las-funciones-flecha-en-javascript-parte-1/
+// Funciones Flecha
+// ()=>{};
+
+let fnA = (saludo) => {
+    return `Hola ${saludo} como estas`;
+}
+let fnB = saludo => {
+    return `Hola ${saludo} como estas`;
+}
+let fnC = saludo => `Hola ${saludo} como estas`;
+let fnD = (saludo) => `Hola ${saludo} como estas`;
