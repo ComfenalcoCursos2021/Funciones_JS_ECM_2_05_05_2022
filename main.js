022153aed59a1633a7a60259ee1ab534d84f4193
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-05-05 20:04:43 -0500

    Separadores Array o Objetos

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
-// http://www.etnassoft.com/2016/06/22/las-funciones-flecha-en-javascript-parte-1/
-// Funciones Flecha
-// ()=>{};
+let nombres = {
+    Nombre1: "Miguel",
+    Nombre2: "Jose",
+    Nombre3: "Carlos", 
+    Nombre4: "Maria",
+};
+let nombres2 = ["Miguel","Jose","Carlos", "Maria"];
 
-let fnA = (saludo) => {
-    return `Hola ${saludo} como estas`;
-}
-let fnB = saludo => {
-    return `Hola ${saludo} como estas`;
-}
-let fnC = saludo => `Hola ${saludo} como estas`;
-let fnD = (saludo) => `Hola ${saludo} como estas`;
+
+let fnA = (Alumnos) => {
+    Object.assign(this, Alumnos);
+    console.log(this.Nombre1);
+    console.log(this.Nombre2);
+    console.log(this.Nombre3);
+    console.log(this.Nombre4);
+};
+let fnB = (Nombre1, Nombre2, Nombre3, Nombre4) => {
+    console.log(Nombre1);
+    console.log(Nombre2);
+    console.log(Nombre3);
+    console.log(Nombre4);
+};
+fnA(nombres);
+console.log("-------------------------------------");
+fnB(...nombres2);
